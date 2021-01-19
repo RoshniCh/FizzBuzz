227ec979771d6356d9a7024e078ace78fcce5912
RoshniCh <roshni.chandrasekar@softwire.com>
2021-01-19 09:39:08 +0000

    fizzbuzz if else if change

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -7,25 +7,19 @@ function fizzbuzz()
         {
             console.log('FizzBuzz'); 
         }
+        else if (i%3 == 0 )
+        {
+           console.log('Fizz');
+        }
+        else if (i%5 == 0 )
+        {
+           console.log('Buzz');
+        }
         else
         {
-            if (i%3 == 0 )
-            {
-                console.log('Fizz');
-            }
-            else 
-            {
-               if (i%5 == 0 )
-               {
-                console.log('Buzz');
-               }
-               else
-               {
-                console.log(i);
-               }
-            }
-        
+           console.log(i);
         }
+    
     }
 }
 
