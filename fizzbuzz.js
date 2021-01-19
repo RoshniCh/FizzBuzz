17e692e8694d03e073016186b5a00cea54425079
RoshniCh <roshni.chandrasekar@softwire.com>
2021-01-19 11:22:36 +0000

    fizz buzz bang bong fezz added 17 changes

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,6 +3,98 @@ function fizzbuzz()
     var i = 0;
     for (i=1; i<300; i++)
     {
+    if (i%17 == 0) /*code for if multiple of 17*/
+    {
+        /*check for 13 in 3 combo */
+        if (i%13 == 0 && i%3 == 0 && i%5 == 0)
+        {
+            console.log('BuzzFezzFizz'); 
+        }
+        else if (i%13 == 0 && i%3 == 0 && i%7 == 0)
+        {
+            console.log('BangFezzFizz'); 
+        }
+        else if (i%13 == 0 && i%3 == 0 && i%11 == 0)
+        {
+            console.log('BongFezzFizz'); 
+        }
+        else if (i%13 == 0 && i%5 == 0 && i%7 == 0)
+        {
+            console.log('BangBuzzFezz'); 
+        }
+        else if (i%13 == 0 && i%5 == 0 && i%11 == 0)
+        {
+            console.log('BongBuzzFezz'); 
+        }
+        else if (i%13 == 0 && i%7 == 0 && i%11 == 0)
+        {
+            console.log('BongBangFezz'); 
+        }
+/*check for 13 in 2 combo */
+        else if (i%13 == 0 && i%3 == 0)
+        {
+             console.log('FezzFizz'); 
+        }
+        else if (i%13 == 0 && i%5 == 0)
+        {
+             console.log('BuzzFezz'); 
+        }
+        else if (i%13 == 0 && i%7 == 0)
+        {
+             console.log('BangFezz'); 
+        }
+        else if (i%13 == 0 && i%11 == 0)
+        {
+             console.log('BongFezz'); 
+        }
+ /*check for 11 */
+        else if (i%11 == 0)
+        {
+            console.log('Bong'); 
+        }
+ /*check for 7 */       
+        else if (i%3 == 0 && i%5 == 0 && i%7 == 0)
+        {
+            console.log('BangBuzzFizz'); 
+        }
+        else if (i%3 == 0 && i%5 == 0)
+        {
+            console.log('BuzzFizz'); 
+        }
+        else if (i%3 == 0 && i%7 == 0)
+        {
+            console.log('BangFizz'); 
+        }
+        else if (i%5 == 0 && i%7 == 0)
+        {
+            console.log('BangBuzz'); 
+        }
+ /*check for individual numbers */
+        else if (i%3 == 0 )
+        {
+           console.log('Fizz');
+        }
+        else if (i%5 == 0 )
+        {
+           console.log('Buzz');
+        }
+        else if (i%7 == 0 )
+        {
+           console.log('Bang');
+        }
+        else if (i%13 == 0 )
+        {
+           console.log('Fezz');
+        }
+        else
+        {
+           console.log(i);
+        }
+    
+    }
+
+    else /*code for if not multiple of 17*/
+    {
 /*check for 13 in 3 combo */
         if (i%13 == 0 && i%3 == 0 && i%5 == 0)
         {
@@ -26,7 +118,7 @@ function fizzbuzz()
         }
         else if (i%13 == 0 && i%7 == 0 && i%11 == 0)
         {
-            console.log('FezzBangBang'); 
+            console.log('FezzBangBong'); 
         }
 /*check for 13 in 2 combo */
         else if (i%13 == 0 && i%3 == 0)
@@ -91,5 +183,5 @@ function fizzbuzz()
     
     }
 }
-
+}
 fizzbuzz();
